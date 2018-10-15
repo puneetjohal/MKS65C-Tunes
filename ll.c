5bfbb626cfbcf001cdc6e168eae025cbb8bd4688
Puneet <pjohal@stuy.edu>
2018-10-15 00:40:32 -0400

    first attempt

--- ll.c
+++ ll.c
@@ -12,11 +12,12 @@ void print_list(struct song_node * link)
     link = link->next; //go to the next val
   }
 }
-struct song_node * insert_front(struct song_node * link, char * new_name)
+struct song_node * insert_front(struct song_node * link, char * new_name, char * artist)
 {
   struct song_node * ptr = malloc(sizeof(struct song_node));
   //allocate memory for a new struct
   strcpy(ptr->name, new_name); //put the name in its place
+  strcpy(ptr->artist, artist); //put the artist in its place
   ptr->next = link; //put the pointer to the next node in its place
   return ptr; //return new front
 }
@@ -32,21 +33,51 @@ struct song_node * free_list(struct song_node * front)
 
 struct song_node * insert_order(struct song_node * front, char * new_name, char * artist)
 {
-  return NULL;
+  struct song_node* insertPoint = front;
+  struct song_node* pointBefore = NULL;
+  while (insertPoint != NULL){
+    if (strcmp(artist, insertPoint->artist) <= 0){
+      if (strcmp(new_name, insertPoint->name) <= 0){
+        insertPoint = insert_front(insertPoint, new_name, artist);
+        break;
+      }
+    }
+    pointBefore = insertPoint;
+    insertPoint = insertPoint->next;
+  }
+  if (insertPoint == NULL){
+    insertPoint = insert_front(insertPoint, new_name, artist);
+    pointBefore->next = insertPoint
+  }
+  else if (pointBefore){
+    pointBefore->next = insertPoint;
+  }
+  return front;
 }
+
 struct song_node * find(char * title, char * artist)
 {
   return NULL;
 }
+
 struct song_node * random_song()
 {
   return NULL;
 }
+
 struct song_node * by_artist(char * artist)
 {
   return NULL;
 }
+
 struct song_node * remove_song(char * title, char * artist)
 {
   return NULL;
+}
+
+int main(){
+  struct song_node *head = (struct song_node*)malloc(sizeof(struct song_node)); //allocates memory
+  struct song_node song = {"Bohemian Rhapsody", "Queen", NULL};
+  print_list(head);
+  return 0;
 }
