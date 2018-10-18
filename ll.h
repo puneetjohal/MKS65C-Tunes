aab6ec6193a79e42ef8ef338c5c0748633d78099
Puneet <pjohal@stuy.edu>
2018-10-18 04:33:07 -0400

    find

--- ll.h
+++ ll.h
@@ -12,7 +12,7 @@ void print_list(struct song_node * link);
 struct song_node * insert_front(struct song_node * link, char * new_name, char * artist);
 struct song_node * free_list(struct song_node * front);
 struct song_node * insert_order(struct song_node * front, char * new_name, char * artist);
-struct song_node * find(char * title, char * artist);
+struct song_node * find(struct song_node * head, char * title, char * artist);
 struct song_node * random_song();
 struct song_node * by_artist(char * artist);
 struct song_node * remove_song(char * title, char * artist);
