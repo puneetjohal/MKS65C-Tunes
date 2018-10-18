baef597fdc5817e856fbc62eb3650a80c77ab430
Puneet <pjohal@stuy.edu>
2018-10-18 15:32:31 -0400

    tunes

--- tunes.c
+++ tunes.c
@@ -1,11 +1,38 @@
+#include <stdio.h>
 #include "ll.c"
 
-void add_node(){}
-struct song_node * find_song(char * title, char * artist){return NULL;}
-struct song_node * find_by_artist(char * artist){return NULL;}
-void print_letter(){}
-void print_artist(){}
-void print_all(){}
-void shuffle(){}
-void deleter(){}
-void clear(){}
+struct song_node * add_node(struct song_node * title, char * track, char * artist){
+  //return insert_order(head,track,artist);
+}
+
+struct song_node * find_song(struct song_node * title, char * title, char * artist){
+  //return find(head,title,artist);
+}
+
+struct song_node * find_by_artist(struct song_node * table, char * artist){
+  //return by_artist(head, artist);
+}
+
+void print_letter(struct song_node * table, char c){
+
+}
+
+void print_artist(){
+
+}
+
+void print_all(){
+
+}
+
+void shuffle(){
+
+}
+
+void deleter(){
+
+}
+
+void clear(){
+
+}
--- tunes.h
+++ tunes.h
@@ -1,6 +1,6 @@
 #include "ll.c"
 
-void add_node();
+void add_node(struct song_node * head, char * track, char * artist);
 struct song_node * find_song(char * title, char * artist);
 struct song_node * find_by_artist(char * artist);
 void print_letter();
